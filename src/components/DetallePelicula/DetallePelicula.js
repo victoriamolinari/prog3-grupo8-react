43130559aeb0eff5eec50b25e0f885ac5d524d8c
CandelaSegura <msegura@udesa.edu.ar>
2024-09-19 15:38:09 -0300

    DetallePelicula terminado

--- src/components/DetallePelicula/DetallePelicula.js
+++ src/components/DetallePelicula/DetallePelicula.js
@@ -2,36 +2,49 @@ import { Component } from 'react';
 import './styles.css';
 
 class DetallePelicula extends Component {
-    //const{nombre, img, calificacion, estreno, duracion, sinopsis} = id;
     constructor(props) {
         super(props);
         this.state = {
-            movies: null
-        }
+            movie: null,
+            isFavorite: false
+        };
     }
+
     componentDidMount() {
         fetch(`https://api.themoviedb.org/3/movie/${this.props.id}?api_key=31e421d77201e7a1eefe33f85b67fa3b`)
             .then(response => response.json())
             .then(data => {
-                console.log('Datos recibidos:', data); // Imprime los datos recibidos
-                this.setState({ movies: data }, () => {
-                    console.log('Estado actualizado:', this.state.movies); // Verifica el estado actualizado
-                });
+                this.setState({ movie: data });
             })
             .catch(error => console.error('Error: ', error));
-
-            console.log(this.state.movies);
-            
-    }
-    
-    render(){
-        return(
-            <h1>hola</h1>
-
-        )
     }
 
+    toggleFavorite = () => {
+        this.setState(prevState => ({ isFavorite: !prevState.isFavorite }));
     }
-    
+
+    render() {
+        const { movie, isFavorite } = this.state;
+
+        if (!movie) {
+            return <h1>Cargando...</h1>;
+        }
+
+        return (
+            <div className="detalle-pelicula">
+                <h1>{movie.title}</h1>
+                <img src={`https://www.themoviedb.org/t/p/w300_and_h450_bestv2${movie.poster_path}`} alt={movie.title} />
+                <p><strong>Calificación:</strong> {movie.vote_average}</p>
+                <p><strong>Fecha de estreno:</strong> {movie.release_date}</p>
+                <p><strong>Duración:</strong> {movie.runtime} minutos</p>
+                <p><strong>Sinópsis:</strong> {movie.overview}</p>
+                <p><strong>Género:</strong> {movie.genres.map(genre => genre.name).join(', ')}</p>
+                <button onClick={this.toggleFavorite}>
+                    {isFavorite ? 'Quitar de Favoritos' : 'Agregar a Favoritos'}
+                </button>
+            </div>
+        );
+    }
+}
 
 export default DetallePelicula;
